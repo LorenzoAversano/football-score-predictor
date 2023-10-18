7593bf54f5ea667d5951dbe00600f78747adae56
Nico <nicobdx@icloud.com>
2023-10-18 16:48:45 +0200

    :sparkles: feat(calcul-score-prediction) : Add logic for predict score #2

--- src/index.ts
+++ src/index.ts
@@ -3,28 +3,21 @@ import { calculScorePrediction } from "./libs/calcul-score-prediction";
 export const predictScore = async ({
   teamA,
   teamB,
-  simlulationCount,
 }: {
   teamA: string;
   teamB: string;
-  simlulationCount: number;
 }) => {
-  //TODO: implement
-  console.log({ teamA, teamB, simlulationCount });
   const teamAData = await getTeamData(teamA);
   const teamBData = await getTeamData(teamB);
   const scorePrediction = calculScorePrediction({
     teamAData,
     teamBData,
-    simlulationCount,
   });
   return scorePrediction;
-
 };
 
-
-predictScore({
-  teamA: "Malte",
-  teamB: "Espagne",
-  simlulationCount: 100,
-})
+// example
+// predictScore({
+//   teamA: "Malte",
+//   teamB: "Espagne",
+// }).then((scorePrediction) => console.log(scorePrediction));
---
+++ src/models/team-data.ts
@@ -0,0 +1,7 @@
+export type TeamData = {
+  name: string;
+  goalsScored: number;
+  goalsTaken: number;
+  numberOfGamesPlayed: number;
+  lastGames: boolean[];
+};
