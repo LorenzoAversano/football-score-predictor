ad545b5a286e517873fb3bb545044465fc127ca1
Nico <nicobdx@icloud.com>
2023-10-18 11:54:11 +0200

    :building_construction: build package structure

---
+++ src/libs/calcul-score-prediction.ts
@@ -0,0 +1,12 @@
+export const calculScorePrediction = ({
+  teamAData,
+  teamBData,
+  simlulationCount,
+}: {
+  teamAData: string; //change type
+  teamBData: string;
+  simlulationCount: number;
+}) => {
+  //TODO: implement it
+  console.log({ teamAData, teamBData, simlulationCount });
+};
---
+++ src/libs/get-team-data.ts
@@ -0,0 +1,5 @@
+export const getTeamData = async (teamName: string): Promise<string> => {
+  //TODO: implement,scrapping or api
+  console.log({ teamName });
+  return "teamData";
+};
